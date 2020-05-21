cbbd6187f3ed6f99a243fbdb0c3f3bcb07338188
BaoDoan <baodoan@BaoDoans-MacBook-Pro.local>
2020-05-21 11:50:37 +0700

    routing v1

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,12 @@ const {requireSignin} = require('../controllers/auth')
 const { userById } = require("../controllers/user");
 //router
 
+router.param('userId', userById)
+
 router.get('/secret/:userId', requireSignin, (req, res)=>{
     res.json({
         user: req.profile
     })
 } )
-
-router.param('userId', userById)
 
 module.exports = router;
