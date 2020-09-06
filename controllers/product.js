320abbca0269acf5b196145054e768e04b4c52fa
BaoDoan <baodoan@BaoDoans-MacBook-Pro.local>
2020-09-06 22:39:49 +0700

    update

--- controllers/product.js
+++ controllers/product.js
@@ -5,7 +5,9 @@ const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.productById = (req, res, next, id) => {
-  Product.findById(id).exec((err, product) => {
+  Product.findById(id)
+  .populate('category')
+  .exec((err, product) => {
     if (err || !product) {
       return res.status(400).json({
         error: "Product not found",
@@ -256,11 +258,36 @@ exports.listBySearch = (req, res) => {
     });
 };
 
-
-exports.photo = (req, res, next) =>{
-  if(req.product.photo.data){
-    res.set('Content-Type', req.product.photo.contentType)
-    return res.send(req.product.photo.data)
+exports.photo = (req, res, next) => {
+  if (req.product.photo.data) {
+    res.set("Content-Type", req.product.photo.contentType);
+    return res.send(req.product.photo.data);
   }
   next();
-}
+};
+
+exports.listSearch = (req, res) => {
+  //create query object to hold search value and category value
+  const query = {};
+  // assign search value to query.name
+
+  if (req.query.search) {
+    query.name = { $regex: req.query.search, $options: "i" };
+    // assign category value to query.category
+    if (req.query.category && req.query.category != "All") {
+      query.category = req.query.category;
+    }
+    // find the product based on query object with 2 properties
+    // search and category
+
+    console.log("query");
+    console.log(query);
+
+    Product.find(query, (err, products) => {
+      if (err) {
+        return res.status(400).json({ error: errorHandler(err) });
+      }
+      res.json(products);
+    }).select("-photo");
+  }
+};
--- routes/product.js
+++ routes/product.js
@@ -3,21 +3,44 @@ const router = express.Router();
 
 const { userById } = require("../controllers/user");
 
-const { create, productById, 
-        read, remove, update, 
-        list, listRelated,
-        listCategories, listBySearch, photo } = require("../controllers/product");
+const {
+  create,
+  productById,
+  read,
+  remove,
+  update,
+  list,
+  listRelated,
+  listCategories,
+  listBySearch,
+  photo,
+  listSearch,
+} = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
-router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
-router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update);
-router.get('/products', list)
-router.get('/products/related/:productId', listRelated)
-router.get('/products/categories', listCategories);
-router.post('/products/by/search', listBySearch);
-router.get('/product/photo/:productId',photo)
+router.delete(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.put(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
+);
+router.get("/products", list);
+router.get("/products/search", listSearch);
+
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.param("userId", userById);
 router.param("productId", productById);
