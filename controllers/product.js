f721281aab10a802b5cb49bfabed2a52eaf56a55
BaoDoan <baodoan@BaoDoans-MacBook-Pro.local>
2020-10-14 21:38:24 +0700

    everything

--- controllers/product.js
+++ controllers/product.js
@@ -6,20 +6,21 @@ const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.productById = (req, res, next, id) => {
   Product.findById(id)
-  .populate('category')
-  .exec((err, product) => {
-    if (err || !product) {
-      return res.status(400).json({
-        error: "Product not found",
+      .populate('category')
+      .exec((err, product) => {
+          if (err || !product) {
+              return res.status(400).json({
+                  error: 'Product not found'
+              });
+          }
+          req.product = product;
+          next();
       });
-    }
-    req.product = product;
-    next();
-  });
 };
 
 exports.read = (req, res) => {
   req.product.photo = undefined;
+  console.log(req.product);
   return res.json(req.product);
 };
 
@@ -94,57 +95,39 @@ exports.create = (req, res) => {
 
 exports.update = (req, res) => {
   let form = new formidable.IncomingForm();
-
   form.keepExtensions = true;
-
   form.parse(req, (err, fields, files) => {
-    if (err) {
-      return res.status(400).json({
-        err: "Image could not be uploaded",
-      });
-    }
-
-    // check for all fields
-    const { name, description, price, category, quantity, shipping } = fields;
-
-    if (
-      !name ||
-      !description ||
-      !price ||
-      !category ||
-      !quantity ||
-      !shipping
-    ) {
-      return res.status(400).json({
-        error: "All fields are required",
-      });
-    }
-
-    let product = req.product;
-    product = _.extend(product, fields);
-
-    // 1kb = 1000
-    // 1mb = 1000000
-
-    if (files.photo) {
-      console.log("FILE PHOTO: ", files.photo);
-      if (files.photo.size > 1000000) {
-        return res.status(400).json({
-          error: "Image should be less than 1mb in size",
-        });
-      }
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
-    }
-
-    product.save((err, result) => {
       if (err) {
-        return res.status(400).json({
-          error: errorHandler(err),
-        });
+          return res.status(400).json({
+              error: 'Image could not be uploaded'
+          });
       }
-      res.json(result);
-    });
+
+      let product = req.product;
+      product = _.extend(product, fields);
+
+      // 1kb = 1000
+      // 1mb = 1000000
+
+      if (files.photo) {
+          // console.log("FILES PHOTO: ", files.photo);
+          if (files.photo.size > 1000000) {
+              return res.status(400).json({
+                  error: 'Image should be less than 1mb in size'
+              });
+          }
+          product.photo.data = fs.readFileSync(files.photo.path);
+          product.photo.contentType = files.photo.type;
+      }
+
+      product.save((err, result) => {
+          if (err) {
+              return res.status(400).json({
+                  error: errorHandler(err)
+              });
+          }
+          res.json(result);
+      });
   });
 };
 
@@ -154,24 +137,25 @@ exports.update = (req, res) => {
  * by arrival = /products?sortBy=createdAt&order=des&limit=4
  * if no params are sent, then all products are returned
  */
+
 exports.list = (req, res) => {
-  let order = req.query.order ? req.query.order : "asc";
-  let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
   let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
   Product.find()
-    .select("-photo")
-    .populate("category")
-    .sort([[sortBy, order]])
-    .limit(limit)
-    .exec((err, products) => {
-      if (err) {
-        res.status(400).json({
-          error: "Products not found",
-        });
-      }
-      res.send(products);
-    });
+      .select('-photo')
+      .populate('category')
+      .sort([[sortBy, order]])
+      .limit(limit)
+      .exec((err, products) => {
+          if (err) {
+              return res.status(400).json({
+                  error: 'Products not found'
+              });
+          }
+          res.json(products);
+      });
 };
 
 /**
@@ -194,16 +178,15 @@ exports.listRelated = (req, res) => {
     });
 };
 
+
 exports.listCategories = (req, res) => {
-  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
-  let category = req.query.category ? req.query.category : null;
-  Product.distinct("category", {}, (err, categories) => {
-    if (err) {
-      return res.status(400).json({
-        error: "Categories not found",
-      });
-    }
-    return res.json(categories);
+  Product.distinct('category', {}, (err, categories) => {
+      if (err) {
+          return res.status(400).json({
+              error: 'Categories not found'
+          });
+      }
+      res.json(categories);
   });
 };
 
@@ -290,4 +273,26 @@ exports.listSearch = (req, res) => {
       res.json(products);
     }).select("-photo");
   }
+};
+
+
+
+exports.decreaseQuantity = (req, res, next) => {
+  let bulkOps = req.body.order.products.map(item => {
+      return {
+          updateOne: {
+              filter: { _id: item._id },
+              update: { $inc: { quantity: -item.count, sold: +item.count } }
+          }
+      };
+  });
+
+  Product.bulkWrite(bulkOps, {}, (error, products) => {
+      if (error) {
+          return res.status(400).json({
+              error: 'Could not update product'
+          });
+      }
+      next();
+  });
 };
