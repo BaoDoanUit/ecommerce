350d3aba7c0a33396037e64bad97b6e66e937a72
Doan Le Ngoc Bao <baodoanuit@gmail.com>
2022-07-19 00:45:29 +0700

    change sqlite for test

--- controllers/demo.js
+++ controllers/demo.js
@@ -1,3 +1,9 @@
+const formidable = require('formidable')
+const excel = require("node-xlsx")
+const fs = require("fs")
+const path = require("path")
+const res = require('express/lib/response')
+const AppDAO = require('../models/dao')
 const data = [
     {
         "id": 1
@@ -10,8 +16,103 @@ const data = [
     },
 ]
 
+const dao = new AppDAO('./database.sqlite3')
+
 exports.list = (req, res) => {
 
     res.json(data)
 
+}
+
+exports.upload = async (req, res) => {
+    await import_excel(req, res, executeFile('demo'));
+    return res.status(201);
+}
+
+exports.readSqlite = async (req, res) => {
+    const data = await readData();
+    return res.json(data);
+}
+
+exports.insertNewLog = async (req, res) => {
+    const log = req.body.log || 'new test'
+    console.log(log);
+    await insert(log);
+
+    const data = await readData();
+    return res.json(data);
+}
+
+const readData = () => {
+    return dao.all(`SELECT * FROM projects`)
+}
+
+
+const createTable = () => {   //Hàm tạo bảng này sẽ dùng để tạo ra cấu trúc bảng projects nếu trong file csdl sqlite3 chưa có bảng này.
+    const sql = `
+    CREATE TABLE IF NOT EXISTS projects (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT)`
+    console.log(`create table ${sql}`);
+    dao.runquery(sql)
+}
+
+const insert = (data) => {
+    dao.runquery(`INSERT INTO projects (name) VALUES (?)`,  [data])
+}
+
+const executeFile = (type) => {
+    res.write('received upload:\n\n');
+}
+
+const import_excel = (req, res, next) => {
+    var user_name = '';
+    var fileName = '';
+    var uploadedPath = '';
+    var fileTypeError = false;
+    var form = new formidable.IncomingForm({
+        encoding: 'utf-8',
+        keepExtensions: true,
+        maxFieldsSize: 10 * 1024 * 1024,
+    });
+
+    form.on('field', function (field, value) {
+        if (field == 'user_name') {
+            user_name = value;
+        }
+    });
+
+    form.on('file', function (field, file) {
+        fileName = file.name;
+        uploadedPath = file.path
+        if (file.type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+            fileTypeError = true;
+        }
+    });
+
+    form.on('end', async function () {
+        var obj = excel.parse(uploadedPath);
+        var excelObj = obj[0].data;
+        console.log('before parse array')
+        console.log(obj[0].data);
+        var mac_array = [];
+        for (var i in excelObj) {
+            var value = excelObj[i];
+            for (var j in value) {
+                mac_array.push(value[j]);
+            }
+        }
+        console.log(mac_array);
+    });
+
+    form.on('error', function (err) {
+        res.send({ ret_code: -1, ret_msg: 'FAILED', extra: "上传出错" });
+    });
+
+
+    form.on('aborted', function () {
+        res.send({ ret_code: -1, ret_msg: 'FAILED', extra: "放弃上传" });
+    });
+
+    form.parse(req);
 }
--- routes/demo.js
+++ routes/demo.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
-const {list} = require("../controllers/demo");
+const bodyParser = require('body-parser')
+const excelParser = bodyParser.raw({
+    type: 'application/octet-stream'
+})
+const { list, upload, readSqlite, insertNewLog } = require("../controllers/demo");
 
 router.get('/demos', list);
+router.post('/upload', excelParser, upload)
+router.get('/readSqlite', readSqlite);
+router.post('/insertlog', insertNewLog);
 
-module.exports =  router
+module.exports = router
