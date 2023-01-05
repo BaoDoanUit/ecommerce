a433121e824d67a92cba166362d805c189db0dcf
Doan Le Ngoc Bao <baodoanuit@gmail.com>
2023-01-05 08:08:55 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-const express = require("express");
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const expressValidator = require('express-validator');
+//import routes
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+const catgoryRoutes = require('./routes/category')
+const productRoutes = require('./routes/product')
+const orderRoutes = require('./routes/order')
+const demoRoutes = require('./routes/demo')
+
+//app
 const app = express();
-const product = require("./api/product");
 
-app.use(express.json({ extended: false }));
+//db
+// mongoose.connect(process.env.DATABASE,{
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+// }).then(()=>console.log('DB Connected'))
 
-app.use("/api/product", product);
+//middleware
+// app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
+//routes middleware
+
+// app.use('/api',authRoutes);
+// app.use('/api',userRoutes);
+// app.use('/api',catgoryRoutes);
+// app.use('/api',productRoutes);
+// app.use('/api',orderRoutes);
+app.use('/api',demoRoutes);
+
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port} `)
+});
+
