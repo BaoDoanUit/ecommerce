b5927a91ffd325e93d98ca2c46b830f67622a338
Doan Le Ngoc Bao <baodoanuit@gmail.com>
2023-02-20 21:31:57 +0700

    update ngrok

--- services/gatewayservices.js
+++ services/gatewayservices.js
@@ -1,5 +1,5 @@
 // const API = 'https://3eee-2402-800-62f8-3d57-b99e-f92e-41db-5ad0.ap.ngrok.io/'
-const API = 'https://f65e-2402-800-62f8-3d57-3285-a9ff-fe22-a2ae.ap.ngrok.io'
+const API = 'https://f65e-2402-800-62f8-3d57-3285-a9ff-fe22-a2ae.ap.ngrok.io/'
 const URL_GET_TOKEN = API + 'authenticate'
 const axios = require('axios');
 
@@ -9,10 +9,10 @@ exports.authenticate = (user, res) => {
       .then(response => {
         console.log(response);
         const token = response.data
-        return res.status(200).data(token);
+        return res.status(200).json(token);
       })
       .catch(error => {
         console.log(error);
-        return res.status(400).data(error);
+        return res.status(400).json(error);
       });
 }
