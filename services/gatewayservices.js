64b1a02be0dc20214b473695ef3f6b257ec596f6
Doan Le Ngoc Bao <baodoanuit@gmail.com>
2023-02-21 22:55:56 +0700

    update localtunnel

--- services/gatewayservices.js
+++ services/gatewayservices.js
@@ -1,5 +1,6 @@
 // const API = 'https://3eee-2402-800-62f8-3d57-b99e-f92e-41db-5ad0.ap.ngrok.io/'
-const API = 'https://f65e-2402-800-62f8-3d57-3285-a9ff-fe22-a2ae.ap.ngrok.io/'
+// const API = 'https://f65e-2402-800-62f8-3d57-3285-a9ff-fe22-a2ae.ap.ngrok.io/'
+const API = 'https://slimy-masks-notice-116-96-78-80.loca.lt/'
 const URL_GET_TOKEN = API + 'authenticate'
 const axios = require('axios');
 
